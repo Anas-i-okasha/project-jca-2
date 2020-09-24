5e497cc70a068dc203c07c92ee2a33c5d610806d
Anas-i-okasha <okasha.i.anas@gmail.com>
2020-09-25 00:10:23 +0300

    finished second project

--- Data.js
+++ Data.js
@@ -1,57 +1,57 @@
 const videos = [
-    {
-      id: "7yLxxyzGiko",
-      title: "Kittens walk with a tiny chicken",
-      thumbnail: "https://img.youtube.com/vi/7yLxxyzGiko/maxresdefault.jpg",
-    },
-    {
-      id: "rS6cvjFXg9s",
-      title: "Cute kitten named Garfield washes its paws and prepares to sleep.",
-      thumbnail: "https://img.youtube.com/vi/rS6cvjFXg9s/maxresdefault.jpg",
-    },
-    {
-      id: "BFNekjEgvuk",
-      title: "Kitten sleeps sweetly with the Chicken 🐥",
-      thumbnail: "https://img.youtube.com/vi/BFNekjEgvuk/maxresdefault.jpg",
-    },
-    {
-      id: "te1fHMaw4UY",
-      title: "White kitten and white tiny bunnies 🐰 | It's so Сute! 🥰",
-      thumbnail: "https://img.youtube.com/vi/te1fHMaw4UY/maxresdefault.jpg",
-    },
-    {
-      id: "MFyUZqSknvw",
-      title: "Kittens meets and walk with a cute white bunny",
-      thumbnail: "https://img.youtube.com/vi/MFyUZqSknvw/maxresdefault.jpg",
-    },
-    {
-      id: "hlajmIPiphk",
-      title: "Mom Cat playing and talking to her Cute Meowing baby Kittens",
-      thumbnail: "https://img.youtube.com/vi/hlajmIPiphk/maxresdefault.jpg",
-    },
-    {
-      id: "Cz47GimSxIc",
-      title: "Funny Kitten wags its tail like a Puppy",
-      thumbnail: "https://img.youtube.com/vi/Cz47GimSxIc/maxresdefault.jpg",
-    },
-    {
-      id: "RmpuhbdnAfQ",
-      title: "Kitty Coco and her Sleepy younger brother 🥰",
-      thumbnail: "https://img.youtube.com/vi/RmpuhbdnAfQ/maxresdefault.jpg",
-    },
-  ];
-  
-  const main = $(`<div class = "first"></div>`)
+  {
+    id: "7yLxxyzGiko",
+    title: "Kittens walk with a tiny chicken",
+    thumbnail: "https://img.youtube.com/vi/7yLxxyzGiko/maxresdefault.jpg",
+  },
+  {
+    id: "rS6cvjFXg9s",
+    title: "Cute kitten named Garfield washes its paws and prepares to sleep.",
+    thumbnail: "https://img.youtube.com/vi/rS6cvjFXg9s/maxresdefault.jpg",
+  },
+  {
+    id: "BFNekjEgvuk",
+    title: "Kitten sleeps sweetly with the Chicken 🐥",
+    thumbnail: "https://img.youtube.com/vi/BFNekjEgvuk/maxresdefault.jpg",
+  },
+  {
+    id: "te1fHMaw4UY",
+    title: "White kitten and white tiny bunnies 🐰 | It's so Сute! 🥰",
+    thumbnail: "https://img.youtube.com/vi/te1fHMaw4UY/maxresdefault.jpg",
+  },
+  {
+    id: "MFyUZqSknvw",
+    title: "Kittens meets and walk with a cute white bunny",
+    thumbnail: "https://img.youtube.com/vi/MFyUZqSknvw/maxresdefault.jpg",
+  },
+  {
+    id: "hlajmIPiphk",
+    title: "Mom Cat playing and talking to her Cute Meowing baby Kittens",
+    thumbnail: "https://img.youtube.com/vi/hlajmIPiphk/maxresdefault.jpg",
+  },
+  {
+    id: "Cz47GimSxIc",
+    title: "Funny Kitten wags its tail like a Puppy",
+    thumbnail: "https://img.youtube.com/vi/Cz47GimSxIc/maxresdefault.jpg",
+  },
+  {
+    id: "RmpuhbdnAfQ",
+    title: "Kitty Coco and her Sleepy younger brother 🥰",
+    thumbnail: "https://img.youtube.com/vi/RmpuhbdnAfQ/maxresdefault.jpg",
+  },
+];
+
+const main = $(`<div class = "first"></div>`)
 const body = $('body')
 body.append(main)
 
-videos.forEach(function(element){
-  
-const secD = $(`<div class="sec"> <img class="img" src = "${videos.thumbnail}"> <p>${videos.title} </p> </div>`)
-$('.img').click(function(){
-  
-$('.video').attr(`src`, `${videos.id}`)
-});
-main.append(secD)
+videos.forEach(function (elm){
+
+  const secD = $(`<div class="sec"> <img class="img" src = "${elm.thumbnail}"> <p>${elm.title} </p> </div>`)
+  $('.img').click(function () {
+
+    $('.video').attr( 'src' , `https://www.youtube.com/embed/${elm.id}`)
+  });
+  main.append(secD)
 
 });
